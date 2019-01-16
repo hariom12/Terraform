1cb0ad932d13173a158788d8821dbbf32bc0e273
hariom12 <hariom0693@gmail.com>
2019-01-17 07:34:10 +1100

    Updated the code

--- main.tf
+++ main.tf
@@ -11,25 +11,19 @@ variable "service_description" {
   type    = "string"
   default = "My awesome GO App"
 }
-
-# Create a VPC to launch our instances into
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
 }
 
-# Create an internet gateway to give our subnet access to the outside world
 resource "aws_internet_gateway" "default" {
   vpc_id = "${aws_vpc.default.id}"
 }
-
-# Grant the VPC internet access on its main route table
 resource "aws_route" "internet_access" {
   route_table_id         = "${aws_vpc.default.main_route_table_id}"
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = "${aws_internet_gateway.default.id}"
 }
 
-# Create a subnet for the ELB & EC2 intances
 resource "aws_subnet" "default" {
   vpc_id                  = "${aws_vpc.default.id}"
   availability_zone       = "${var.aws_region}"
