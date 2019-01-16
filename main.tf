46780eead81842b184a323eaab353c2203431af7
hariom12 <hariom0693@gmail.com>
2019-01-17 07:43:54 +1100

    removed the default vpc for code

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_route" "internet_access" {
 }
 
 resource "aws_subnet" "default" {
-  vpc_id                  = "${aws_vpc.default.id}"
+  vpc_id                  = "${aws_vpc.customvpc.id}"
   availability_zone       = "${var.aws_region}"
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
